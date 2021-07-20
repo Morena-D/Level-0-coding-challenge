94d35246216c3d38ae39fde2e40a2db71a506a01
Morena Dikutle <morenadikutle@gmail.com>
2021-07-20 10:45:04 +0200

    Removed comments

--- Task0_10.py
+++ Task0_10.py
@@ -1,10 +1,10 @@
 def compare_words(word_1, word_2):
     results = ""
-    for character in word_1.lower():    #Iterating within first string
+    for character in word_1.lower():    
         if character in word_2.lower():
             if character in results:
-                    continue        #continue if character is in results
-            results += character      #incrementing results with characters that exist in the two string  
+                    continue        
+            results += character        
     print(f"Common words: {(', '.join(results))}.")
 
 word1 = "stink"

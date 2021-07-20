de2107c1c1363264d87c64f416198d7d383b937d
Morena Dikutle <morenadikutle@gmail.com>
2021-07-20 10:52:53 +0200

    Removed comments

--- Task0_5.py
+++ Task0_5.py
@@ -1,8 +1,8 @@
 import math
 def calarea(a,b,c):
-    s = 0.5*(a+b+c)  #formula for the semiperimeter of triangle
+    s = 0.5*(a+b+c)  
 
-    total = s*(s-a)*(s-b)*(s-c)   #calculating the area of the triangle
+    total = s*(s-a)*(s-b)*(s-c)   
     area = math.sqrt(total)
 
     return f"The area of the triangle is: {area}"

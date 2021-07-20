dc7adde03d2806bb84c5aee6f4316ef96918a928
Morena Dikutle <morenadikutle@gmail.com>
2021-07-20 10:50:55 +0200

    Removed comments

--- Task0_7.py
+++ Task0_7.py
@@ -1,13 +1,10 @@
 def celsius_to_fahrenheit(c):
-    fahrenheit = (c * (9/5)) + 32   #Formula for calculating temperature in Fahrenheit
+    fahrenheit = (c * (9/5)) + 32   
     return fahrenheit
 
 def fahrenheit_to_celsius(f):
-    celsius = (f - 32) * (5/9)   #Formula for calculating temperature in celsius
+    celsius = (f - 32) * (5/9)   
     return celsius
 
-
-c = 80
-f = 80
-print(f"The temperature from celsius to Fahrenheit is {celsius_to_fahrenheit(c)}")
-print(f"The temperature from Fahrenheit to Celsius is {fahrenheit_to_celsius(f)}")
+print(f"The temperature from celsius to Fahrenheit is {celsius_to_fahrenheit(80)}")
+print(f"The temperature from Fahrenheit to Celsius is {fahrenheit_to_celsius(80)}")

f701d85de9ca0c7709fc73bc54c41b11a41d932a
Morena Dikutle <morenadikutle@gmail.com>
2021-07-20 10:46:26 +0200

    Removed comments

--- Task0_9.py
+++ Task0_9.py
@@ -1,11 +1,11 @@
 def vowel_identify(name):
     vowels = "aeiou"
     results = ""
-    for character in name.lower():   #iterating within the string
+    for character in name.lower():   
         if character in vowels.lower():
             if character in results:
                 continue
-            results += character        #incrementing results with characters that exists between vowels and string
+            results += character        
     print(f"vowels: {(', '.join(results))}.")
 name = "UMUZI"
 vowel_identify(name)

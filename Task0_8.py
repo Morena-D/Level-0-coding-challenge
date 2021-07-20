508fdbf8e9e792f02930dc23c6cb208226313122
Morena Dikutle <morenadikutle@gmail.com>
2021-07-20 10:48:26 +0200

    Removed comments

--- Task0_8.py
+++ Task0_8.py
@@ -1,6 +1,6 @@
 def time_convert(t):
-    hours = t // 60   #returning only the integer part of the qoutient
-    minutes = t % 60   #returning the remaing part of the qoutient
+    hours = t // 60   
+    minutes = t % 60   
     if hours == 1 and minutes ==1:
         return f"{hours} hour and {minutes} minute"
     if hours == 1:

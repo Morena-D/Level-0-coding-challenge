6d2e03892f71d98bf434e2633062cabd18ff6307
Morena Dikutle <morenadikutle@gmail.com>
2021-07-21 11:01:29 +0200

    Changed built-in function

--- Task0_5.py
+++ Task0_5.py
@@ -1,10 +1,10 @@
-import math
 def calarea(a,b,c):
     s = 0.5*(a+b+c)  
 
     total = s*(s-a)*(s-b)*(s-c)   
-    area = math.sqrt(total)
-
+    sqrt = total ** 0.5
+    area = sqrt
+    
     return f"The area of the triangle is: {area}"
 
 print(calarea(3,4,5))
